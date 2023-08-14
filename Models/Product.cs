c25b91f613dc29483c9111dee66e1dde1cf95bac
Muskan-Sandhay <134076761+Muskan-Sandhay@users.noreply.github.com>
2023-08-14 19:24:43 -0400

    Add files via upload

---
+++ Models/Product.cs
@@ -0,0 +1,25 @@
+﻿/*
+The namespace has .Models to refer to the current folder "Models" 
+*/
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CoolCrafts.WebSite.Models
+{
+    public class Product
+    {
+
+
+        public string? Id { get; set; }
+        public string? Maker { get; set; }
+
+        [JsonPropertyName("img")]
+        public string? Image { get; set; }
+        public string? Url { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public int[]? Ratings { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Product>(this);
+    }
+}
---
+++ pages/about.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ClassCraft.pages
+{
+    public class aboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
